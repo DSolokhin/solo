0c0ee7c1ba2facb776564f0d38608872391e7aca
Dmitry Solokhin <d.solo@bk.ru>
2021-12-07 15:33:10 +0300

    rebase

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,9 +2,7 @@
 # display command line options
 
 count=1
-for param in "$@"; do
-    echo "Next parameter: $param"
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
-
-echo "====="
